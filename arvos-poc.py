4dd7b234960e47661c972677590f1049ac5a2552
Mulugeta Ayalew Tamiru <mulugetayalew@gmail.com>
2022-02-11 09:36:15 +0100

    Improve code readability and add stack traces

--- arvos-poc.py
+++ arvos-poc.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 
-from __future__ import print_function
 import argparse
 from bcc import BPF, USDT, utils
 import ctypes as ct
@@ -26,7 +25,6 @@ class bcolors:
 
 class CallEvent(ct.Structure):
     _fields_ = [
-        ("depth", ct.c_ulonglong),
         ("pid", ct.c_ulonglong),
         ("clazz", ct.c_char * 128),
         ("method", ct.c_char * 128)
@@ -36,7 +34,8 @@ def readResults():
 
     # period
     PERIOD = 10
-    # Vulnerability db
+    
+    # Vulnerability dataset
     f = open('arvos_vfs.json')
     vulnData = json.load(f)
     f.close()
@@ -45,6 +44,14 @@ def readResults():
     # Invoked classes and methods
     invokedClassesFile = 'invoked_classes.csv'
 
+    # Stack trace file
+    stack_trace = "/stack_logs/stack-traces.log"
+    with open(stack_trace) as f:
+        stack_output = f.read()
+
+    stack_array = stack_output.split(os.linesep + os.linesep)
+
+    # Check if scanning has started
     while not os.path.exists(invokedClassesFile):
         print(f"{bcolors.WARNING}Waiting for scanning to start ...{bcolors.ENDC}")
         time.sleep(1)
@@ -52,6 +59,7 @@ def readResults():
     print(f"{bcolors.OKGREEN}Scanning is now started.")
     print(f"{bcolors.OKGREEN}Please wait patiently until we gather the results ...{bcolors.ENDC}")
 
+    # Check if invoked symbols are in the vulnerability dataset
     while 1:
         try:
             dfInvoked = pd.read_csv(invokedClassesFile, header=None)
@@ -61,7 +69,10 @@ def readResults():
 
             last_ts = dfInvoked['timestamp'].iloc[-1]
             first_ts = last_ts - pd.Timedelta(PERIOD, 'seconds')
-            filtered_df = dfInvoked[dfInvoked['timestamp'] >= first_ts]
+            filtered_df_orig = dfInvoked[dfInvoked['timestamp'] >= first_ts]
+            
+            # Remove duplicates
+            filtered_df = filtered_df_orig.drop_duplicates(subset = ['clazz', 'method']).reset_index(drop = True)
 
             # Compare
             for index, row in dfVuln.iterrows():
@@ -70,41 +81,46 @@ def readResults():
                     method_name = item['method_name']
                     dfClass = filtered_df[filtered_df['clazz'].str.contains(class_name)]
                     dfMethod = dfClass[dfClass['method'].str.contains(method_name)]
+
                     if len(dfMethod) > 0:
                         for i, r in dfMethod.iterrows():
-                            print(f"{bcolors.FAIL}Vulnerability found!!!")
+                            print(f"{bcolors.FAIL}Vulnerable symbol invoked!!!")
                             print(f"\t{bcolors.FAIL}Time:{bcolors.ENDC} {r['time']}")
                             print(f"\t{bcolors.FAIL}Vulnerability:{bcolors.ENDC} {row['vulnerability']}")
                             print(f"\t{bcolors.FAIL}Repository:{bcolors.ENDC} {row['repository']}")
-                            print(f"\t{bcolors.FAIL}Invoked Class:{bcolors.ENDC} {row['symbols'][0]['class_name']}")
-                            print(f"\t{bcolors.FAIL}Invoked Method:{bcolors.ENDC} {row['symbols'][0]['method_name']}")
+                            print(f"\t{bcolors.FAIL}Invoked Class:{bcolors.ENDC} {item['class_name']}")
+                            print(f"\t{bcolors.FAIL}Invoked Method:{bcolors.ENDC} {method_name}")
                             print(f"\t{bcolors.FAIL}Confidence:{bcolors.ENDC} {row['confidence']}")
                             print(f"\t{bcolors.FAIL}Spread:{bcolors.ENDC} {row['spread']}")
+
+                            for trace in stack_array:
+                                search_term = class_name.replace('/', '.') + "." + method_name
+                                if search_term in trace:
+                                    print(f"\t{bcolors.FAIL}Stack trace:{bcolors.ENDC}")
+                                    print("\t", trace)
+                                    break
             time.sleep(PERIOD)
         except KeyboardInterrupt:
             print("EXITING ....")
             exit()
 
 def print_event(cpu, data, size):
-    # get event
+    # get event from perf buffer
     event = ct.cast(data, ct.POINTER(CallEvent)).contents
     invokedClass = event.clazz.decode('utf-8', 'replace')
     invokedMethod = event.method.decode('utf-8', 'replace')
 
     output = [datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S"), invokedClass, invokedMethod]
 
+    # Write events to file
     with open('invoked_classes.csv', 'a', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(output)
 
 
 def run_uflow(pid):
-    clazz = ""
-    method = ""
-
     program = """
     struct call_t {
-        u64 depth;                  // first bit is direction (0 entry, 1 return)
         u64 pid;                    // (tgid << 32) + pid from bpf_get_current...
         char clazz[128];
         char method[128];
@@ -112,79 +128,34 @@ def run_uflow(pid):
 
     BPF_PERF_OUTPUT(calls);
     BPF_HASH(entry, u64, u64);
-    """
 
-    prefix_template = """
-    static inline bool prefix_%s(char *actual) {
-        char expected[] = "%s";
-        for (int i = 0; i < sizeof(expected) - 1; ++i) {
-            if (expected[i] != actual[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    """
-
-    if clazz:
-        program += prefix_template % ("class", clazz)
-    if method:
-        program += prefix_template % ("method", method)
-
-    trace_template = """
-    int NAME(struct pt_regs *ctx) {
-        u64 *depth, zero = 0, clazz = 0, method = 0 ;
+    int java_entry(struct pt_regs *ctx) {
+        u64 clazz = 0, method = 0 ;
         struct call_t data = {};
 
-        READ_CLASS
-        READ_METHOD
+        bpf_usdt_readarg(2, ctx, &clazz);
+        bpf_usdt_readarg(4, ctx, &method);
         bpf_probe_read(&data.clazz, sizeof(data.clazz), (void *)clazz);
         bpf_probe_read(&data.method, sizeof(data.method), (void *)method);
 
-        FILTER_CLASS
-        FILTER_METHOD
-
         data.pid = bpf_get_current_pid_tgid();
-        depth = entry.lookup_or_try_init(&data.pid, &zero);
-        if (!depth) {
-            depth = &zero;
-        }
-        data.depth = DEPTH;
-        UPDATE
 
         calls.perf_submit(ctx, &data, sizeof(data));
         return 0;
     }
-    """
-
-    usdt = USDT(pid=pid)
+    """    
 
     probe_name = "method__entry"
     func_name = "java_entry"
-    read_class = "bpf_usdt_readarg(2, ctx, &clazz);"
-    read_method = "bpf_usdt_readarg(4, ctx, &method);"
-    is_return = False
 
-    # global program, trace_template, usdt
-    depth = "*depth + 1" if not is_return else "*depth | (1ULL << 63)"
-    update = "++(*depth);" if not is_return else "if (*depth) --(*depth);"
-    filter_class = "if (!prefix_class(data.clazz)) { return 0; }" \
-        if clazz else ""
-    filter_method = "if (!prefix_method(data.method)) { return 0; }" \
-        if method else ""
-    program += trace_template.replace("NAME", func_name) \
-        .replace("READ_CLASS", read_class) \
-        .replace("READ_METHOD", read_method) \
-        .replace("FILTER_CLASS", filter_class) \
-        .replace("FILTER_METHOD", filter_method) \
-        .replace("DEPTH", depth) \
-        .replace("UPDATE", update)
 
+    # usdt
+    usdt = USDT(pid=pid)
     usdt.enable_probe_or_bail(probe_name, func_name)
 
     bpf = BPF(text=program, usdt_contexts=[usdt])
 
-    bpf["calls"].open_perf_buffer(print_event, page_cnt=8192)
+    bpf["calls"].open_perf_buffer(print_event, page_cnt=8192*8)
 
     while 1:
         try:
