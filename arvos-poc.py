6e8a7218e6c1b47c86de099293fbe9b5247e9681
Mulugeta Ayalew Tamiru <mulugetayalew@gmail.com>
2022-02-14 15:45:06 +0100

    Change the implementation: now using hash tables and single process
    I changed the implementation to improve performance. Now the program used BPF_HASH map to store unique symbols, thus reducing the amount of data transferred from kernel- to user-space. Also, no more need to run two processes.

--- arvos-poc.py
+++ arvos-poc.py
@@ -1,16 +1,17 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
+# @lint-avoid-python-3-compatibility-imports
 
 import argparse
-from bcc import BPF, USDT, utils
-import ctypes as ct
-import csv
 from datetime import datetime
-import time
+from time import sleep
+from bcc import BPF, USDT
 import json
-import pandas as pd
-import multiprocessing as mp
 import os
 
+# period
+PERIOD = 10
+
+# Text colors
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -22,164 +23,118 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Get arguments from command line
+parser = argparse.ArgumentParser(
+    description="Trace Java applications.",
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("pid", type=int, help="process id to attach to")
+parser.add_argument("-v", "--verbose", action="store_true",
+    help="verbose mode: print the BPF program (for debugging purposes)")
+args = parser.parse_args()
 
-class CallEvent(ct.Structure):
-    _fields_ = [
-        ("pid", ct.c_ulonglong),
-        ("clazz", ct.c_char * 128),
-        ("method", ct.c_char * 128)
-    ]
+# BPF program
+program = """
+#define MAX_STRING_LENGTH 160
 
-def read_results():
-    # period
-    PERIOD = 10
-    
-    # Vulnerability dataset
-    f = open('arvos_vfs.json')
-    vulnData = json.load(f)
-    f.close()
-    dfVuln = pd.DataFrame(vulnData)
+struct method_t {
+    char clazz[MAX_STRING_LENGTH];
+    char method[MAX_STRING_LENGTH];
+};
 
-    # Invoked classes and methods
-    invokedClassesFile = 'invoked_classes.csv'
+struct entry_t {
+    u64 pid;
+    struct method_t method;
+};
 
-    # Stack trace file
-    stack_trace = "/stack_logs/stack-traces.log"
-    with open(stack_trace) as f:
-        stack_output = f.read()
+BPF_HASH(counts, struct method_t, u64);
 
-    stack_array = stack_output.split(os.linesep + os.linesep)
+int trace_entry(struct pt_regs *ctx) {
+    u64 clazz = 0, method = 0, val = 0;
+    u64 *valp;
+    struct entry_t data = {0};
+    u64 timestamp = bpf_ktime_get_ns();
+    data.pid = bpf_get_current_pid_tgid();
+    bpf_usdt_readarg(2, ctx, &clazz);
+    bpf_usdt_readarg(4, ctx, &method);
+    bpf_probe_read(&data.method.clazz, sizeof(data.method.clazz),
+                   (void *)clazz);
+    bpf_probe_read(&data.method.method, sizeof(data.method.method),
+                   (void *)method);
 
-    # Check if scanning has started
-    while not os.path.exists(invokedClassesFile):
-        print(f"{bcolors.WARNING}Waiting for scanning to start ...{bcolors.ENDC}")
-        time.sleep(1)
-
-    print(f"{bcolors.OKGREEN}Scanning is now started.")
-    print(f"{bcolors.OKGREEN}Please wait patiently until we gather the results ...{bcolors.ENDC}")
-
-    # Check if invoked symbols are in the vulnerability dataset
-    while 1:
-        try:
-            dfInvoked = pd.read_csv(invokedClassesFile, header=None)
-            dfInvoked.columns = ['time', 'clazz', 'method']
-
-            dfInvoked['timestamp'] = dfInvoked['time'].apply(lambda x: pd.Timestamp(x))
-
-            last_ts = dfInvoked['timestamp'].iloc[-1]
-            first_ts = last_ts - pd.Timedelta(PERIOD, 'seconds')
-            filtered_df_orig = dfInvoked[dfInvoked['timestamp'] >= first_ts]
-            
-            # Remove duplicates
-            filtered_df = filtered_df_orig.drop_duplicates(subset = ['clazz', 'method']).reset_index(drop = True)
-
-            # Compare
-            for index, row in dfVuln.iterrows():
-                for item in row['symbols']:
-                    class_name = item['class_name'].replace('.', '/')
-                    method_name = item['method_name']
-                    dfClass = filtered_df[filtered_df['clazz'].str.contains(class_name)]
-                    dfMethod = dfClass[dfClass['method'].str.contains(method_name)]
-
-                    if len(dfMethod) > 0:
-                        for i, r in dfMethod.iterrows():
-                            print(f"{bcolors.FAIL}Vulnerable symbol invoked!!!")
-                            print(f"\t{bcolors.FAIL}Time:{bcolors.ENDC} {r['time']}")
-                            print(f"\t{bcolors.FAIL}Vulnerability:{bcolors.ENDC} {row['vulnerability']}")
-                            print(f"\t{bcolors.FAIL}Repository:{bcolors.ENDC} {row['repository']}")
-                            print(f"\t{bcolors.FAIL}Invoked Class:{bcolors.ENDC} {item['class_name']}")
-                            print(f"\t{bcolors.FAIL}Invoked Method:{bcolors.ENDC} {method_name}")
-                            print(f"\t{bcolors.FAIL}Confidence:{bcolors.ENDC} {row['confidence']}")
-                            print(f"\t{bcolors.FAIL}Spread:{bcolors.ENDC} {row['spread']}")
-
-                            for trace in stack_array:
-                                search_term = class_name.replace('/', '.') + "." + method_name
-                                if search_term in trace:
-                                    print(f"\t{bcolors.FAIL}Stack trace:{bcolors.ENDC}")
-                                    print("\t", trace)
-                                    break
-            time.sleep(PERIOD)
-        except KeyboardInterrupt:
-            print("EXITING ....")
-            exit()
-
-def print_event(cpu, data, size):
-    # get event from perf buffer
-    event = ct.cast(data, ct.POINTER(CallEvent)).contents
-    invokedClass = event.clazz.decode('utf-8', 'replace')
-    invokedMethod = event.method.decode('utf-8', 'replace')
-
-    output = [datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S"), invokedClass, invokedMethod]
-
-    # Write events to file
-    with open('invoked_classes.csv', 'a', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(output)
-
-
-def run_uflow(pid):
-    program = """
-    struct call_t {
-        u64 pid;
-        char clazz[128];
-        char method[128];
-    };
-
-    BPF_PERF_OUTPUT(calls);
-    BPF_HASH(entry, u64, u64);
-
-    int java_entry(struct pt_regs *ctx) {
-        u64 clazz = 0, method = 0 ;
-        struct call_t data = {};
-
-        bpf_usdt_readarg(2, ctx, &clazz);
-        bpf_usdt_readarg(4, ctx, &method);
-        bpf_probe_read(&data.clazz, sizeof(data.clazz), (void *)clazz);
-        bpf_probe_read(&data.method, sizeof(data.method), (void *)method);
-
-        data.pid = bpf_get_current_pid_tgid();
-
-        calls.perf_submit(ctx, &data, sizeof(data));
-        return 0;
+    valp = counts.lookup_or_try_init(&data.method, &val);
+    if (valp) {
+        ++(*valp);
     }
-    """    
 
-    probe_name = "method__entry"
-    func_name = "java_entry"
-
-
-    # usdt
-    usdt = USDT(pid=pid)
-    usdt.enable_probe_or_bail(probe_name, func_name)
-
-    bpf = BPF(text=program, usdt_contexts=[usdt])
-
-    bpf["calls"].open_perf_buffer(print_event, page_cnt=8192*8)
-
-    while 1:
-        try:
-            bpf.perf_buffer_poll()
-        except KeyboardInterrupt:
-            print("EXITING ....")
-            exit()
-
-examples = """examples:
-    ./arvos-poc 185                # trace Java method calls in process 185
+    //entry.update(&data, &timestamp);
+    return 0;
+}
 """
 
-parser = argparse.ArgumentParser(
-    description="Trace method execution flow in Java.",
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog=examples)
-parser.add_argument("pid", type=int, help="process id to attach to")
-args = parser.parse_args()
-pid = args.pid
+# usdt
+usdt = USDT(pid=args.pid)
+entry_probe = "method__entry"
+usdt.enable_probe_or_bail(entry_probe, "trace_entry")
 
-# Multi-processing
+if args.verbose:
+    if args.verbose and usdt:
+        print(usdt.get_text())
+    print(program)
 
-p = mp.Process(target=run_uflow, args=(pid,))
-q = mp.Process(target=read_results)
-p.start()
-q.start()
-p.join()
-q.join()
+# Attach BPF program to USDT probes
+bpf = BPF(text=program, usdt_contexts=[usdt] if usdt else [])
+
+# Load vulnerability dataset
+f = open('arvos_vfs.json')
+vulnData = json.load(f)
+f.close()
+
+# Stack trace file
+stack_trace = "/stack_logs/stack-traces.log"
+with open(stack_trace) as f:
+    stack_output = f.read()
+# Load it into an array
+stack_array = stack_output.split(os.linesep + os.linesep)
+
+
+print("\nTracing Java calls in process %d and scanning for vulnerable symbols ... Ctrl-C to quit." % (args.pid))
+
+# Loop until exit
+while True:
+    try:
+        sleep(PERIOD)
+
+        invoked_class_list = []
+        invoked_method_list = []
+    
+        for k,v in bpf["counts"].items():
+            invoked_class_list.append(k.clazz.decode('utf-8', 'replace').replace('/', '.'))
+            invoked_method_list.append(k.method.decode('utf-8', 'replace'))
+
+
+        for item in vulnData:
+            for sym in item['symbols']:
+                for i in range(len(invoked_class_list)):
+                    if sym['class_name'] in invoked_class_list[i] and sym['method_name'] in invoked_method_list[i]:
+                    
+                        t_now = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S")
+
+                        print(f"\n{bcolors.FAIL}Vulnerable symbol invoked!!!")
+                        print(f"\t{bcolors.FAIL}Time:{bcolors.ENDC} {t_now}")
+                        print(f"\t{bcolors.FAIL}Vulnerability:{bcolors.ENDC} {item['vulnerability']}")
+                        print(f"\t{bcolors.FAIL}Repository:{bcolors.ENDC} {item['repository']}")
+                        print(f"\t{bcolors.FAIL}Invoked Class:{bcolors.ENDC} {sym['class_name']}")
+                        print(f"\t{bcolors.FAIL}Invoked Method:{bcolors.ENDC} {sym['method_name']}")
+                        print(f"\t{bcolors.FAIL}Confidence:{bcolors.ENDC} {item['confidence']}")
+                        print(f"\t{bcolors.FAIL}Spread:{bcolors.ENDC} {item['spread']}")
+
+                        for trace in stack_array:
+                            search_term = sym['class_name'] + "." + sym['method_name']
+                            if search_term in trace:
+                                print(f"\t{bcolors.FAIL}Stack trace:{bcolors.ENDC}")
+                                print("\t", trace)
+                                break
+
+    except KeyboardInterrupt:
+        print("EXITING ...")
+        exit()
